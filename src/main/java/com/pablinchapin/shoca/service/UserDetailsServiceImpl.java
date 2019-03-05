9e6611bfe37ddb300e183c6553f9fc3da2e3d061
pablinchapin <pablinchapin@gmail.com>
2019-03-05 16:39:49 -0600

    Agregando las templates iniciales con bootstrap y finalizando el servicio de userdetail

--- src/main/java/com/pablinchapin/shoca/service/UserDetailsServiceImpl.java
+++ src/main/java/com/pablinchapin/shoca/service/UserDetailsServiceImpl.java
@@ -5,19 +5,64 @@
  */
 package com.pablinchapin.shoca.service;
 
+import com.pablinchapin.shoca.dao.AccountDAO;
+import com.pablinchapin.shoca.entity.Account;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 /**
  *
  * @author pvargas
  */
+
+@Service
 public class UserDetailsServiceImpl implements UserDetailsService{
 
+    @Autowired
+    private AccountDAO accountDAO;
+    
     @Override
-    public UserDetails loadUserByUsername(String string) throws UsernameNotFoundException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        
+        Account account = accountDAO.findAccount(username);
+        
+        System.out.println("Account= " +account);
+        
+        if(account == null){
+            throw new UsernameNotFoundException("User " +username+ " was not found in DB");
+        }
+        
+        String role = account.getUserRole();
+        
+        List<GrantedAuthority> grantList = new ArrayList<>();
+        
+        GrantedAuthority authority = new SimpleGrantedAuthority(role);
+        grantList.add(authority);
+        
+        boolean enabled = account.isActive();
+        boolean accountNonExpired = true;
+        boolean credentialsNonExpired = true;
+        boolean accountNonLocked = true;
+        
+        UserDetails userDetails = new User(
+                account.getUserName(),
+                account.getEncryptedPassword(),
+                enabled,
+                accountNonExpired,
+                credentialsNonExpired,
+                accountNonLocked, 
+                grantList
+        );
+        
+        return userDetails;
     }
     
 }
